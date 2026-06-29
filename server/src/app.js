300d59dd5b76410e55ca654ac4aed25816bd0b19
developerdarshana123 <23115667@hitdoon.com>
2026-06-29 13:58:33 +0530

    add cron joib

--- server/src/app.js
+++ server/src/app.js
@@ -78,6 +78,9 @@ app.use(
 app.use(cookieParser());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.get("/keep-alive", (req, res) => {
+  res.status(200).json({ status: "alive" });
+});
 
 if (process.env.NODE_ENV !== "test") {
   app.use(morgan("combined"));
