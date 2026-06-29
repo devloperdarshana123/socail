41e7b0013bf1c2f79bf43455cfdf44e151703839
developerdarshana123 <23115667@hitdoon.com>
2026-06-29 14:40:30 +0530

    corn job one

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import compression from "compression";
 import morgan from "morgan";
+import logger from "./config/logger.js";
 
 import {
   authRouteLimiter,
@@ -78,8 +79,10 @@ app.use(
 app.use(cookieParser());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+
 app.get("/keep-alive", (req, res) => {
-  res.status(200).json({ status: "alive" });
+  logger.info("Keep-alive ping received");
+  res.status(200).send("OK");
 });
 
 if (process.env.NODE_ENV !== "test") {
